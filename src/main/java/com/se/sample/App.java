cdfd79561741458eb28fc282601a14e04c976652
Evgeniy Skiba <eskiba@s-pro.io>
2020-02-18 19:15:00 +0200

    Update. Added await termination.

--- src/main/java/com/se/sample/App.java
+++ src/main/java/com/se/sample/App.java
@@ -2,6 +2,8 @@ package com.se.sample;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
@@ -18,31 +20,40 @@ public class App
 
             Counter counter = new Counter();
             int cores = Runtime.getRuntime().availableProcessors();
-            ExecutorService threadPool = Executors.newFixedThreadPool(cores);
+            ExecutorService executorService = Executors.newFixedThreadPool(cores);
 
-            threadPool.execute(new Decrement("Decrement 1", counter,locker));
-            threadPool.execute(new Increment("Increment 2", counter,locker));
 
-            threadPool.execute(new Decrement("Decrement 3", counter,locker));
-            threadPool.execute(new Increment("Increment 3", counter,locker));
+            executorService.submit(new Decrement("Decrement 1", counter, locker));
+            executorService.submit(new Increment("Increment 2", counter, locker));
 
-            threadPool.execute(new Increment("Increment 1", counter,locker));
-            threadPool.execute(new Decrement("Decrement 2", counter,locker));
+            executorService.submit(new Decrement("Decrement 3", counter, locker));
+            executorService.submit(new Increment("Increment 3", counter, locker));
 
-            threadPool.execute(new Increment("Increment 2", counter,locker));
-            threadPool.execute(new Decrement("Decrement 4", counter,locker));
-            threadPool.execute(new Increment("Increment 4", counter,locker));
+            executorService.submit(new Increment("Increment 1", counter, locker));
+            executorService.submit(new Decrement("Decrement 2", counter, locker));
 
-            threadPool.execute(new Decrement("Decrement 5", counter,locker));
-            threadPool.execute(new Increment("Increment 5", counter,locker));
+            executorService.submit(new Increment("Increment 2", counter, locker));
+            executorService.submit(new Decrement("Decrement 4", counter, locker));
+            executorService.submit(new Increment("Increment 4", counter, locker));
 
-            threadPool.execute(new Decrement("Decrement 6", counter,locker));
-            threadPool.execute(new Increment("Increment 6", counter,locker));
+            executorService.submit(new Decrement("Decrement 5", counter, locker));
+            executorService.submit(new Increment("Increment 5", counter, locker));
 
-            threadPool.shutdown();
+            executorService.submit(new Decrement("Decrement 6", counter, locker));
+            executorService.submit(new Increment("Increment 6", counter, locker));
 
-            while(true){
+            executorService.shutdown();
 
+
+            try {
+                //ждем 10 мин - иначе принудительно завершаем
+                if (!executorService.awaitTermination(10, TimeUnit.MINUTES)) {
+
+                    executorService.shutdownNow();
+                }
+            } catch (InterruptedException ex) {
+                executorService.shutdownNow();
+                Thread.currentThread().interrupt();
             }
         }
         catch (Exception e)
